1e6f6f3533f2ded0e6f5f6f17c0187cdf4988bec
Pedroassuncao <alwaysdeck@gmail.com>
2021-06-23 13:54:46 +0100

    2º aula

--- src/screens/SignIn/index.tsx
+++ src/screens/SignIn/index.tsx
@@ -3,22 +3,25 @@ import {
   View, 
   Text, 
   Image,  
-  StatusBar,
 } from 'react-native';
 
 import IllustrationImg from '../../assets/illustration.png';
 import { styles } from './styles';
 
 import { ButtonIcon } from '../../components/ButtonIcon';
+import { useNavigation } from '@react-navigation/native';
+
 
 export function SignIn(){
+
+  const navigation = useNavigation();
+
+  function handleSignIn() {
+    navigation.navigate('Home');
+  }
+
   return(
     <View style={styles.container}>
-      <StatusBar 
-        barStyle="light-content"
-        backgroundColor="transparent"
-        translucent
-      />
       
       <Image 
         source={IllustrationImg} 
@@ -28,19 +31,19 @@ export function SignIn(){
 
       <View style={styles.content}>
         <Text style={styles.title}>
-          Organize {`\n`}
-          suas jogatinas {`\n`} 
-          facilmente
+          Conecta-te {'\n'}
+          e organiza as tuas {'\n'}
+          jogatinas
         </Text>
 
         <Text style={styles.subtitle}>
-          Crie grupos para jogar seus games {`\n`} 
+          Cria grupos para jogar os teus jogos {'\n'} 
           favoritos com seus amigos
         </Text>
 
         <ButtonIcon 
           title="Entrar com Discord"
-          activeOpacity={0.7}
+          onPress={handleSignIn}
         />                
                
       </View>
